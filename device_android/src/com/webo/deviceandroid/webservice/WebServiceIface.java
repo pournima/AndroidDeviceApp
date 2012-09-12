aab7cc2cd46f74c2e4e45ae0ad34d8f380127092
pournima <pournima@weboniselab.com>
2012-09-12 17:39:48 +0530

    commit the app

---
+++ device_android/src/com/webo/deviceandroid/webservice/WebServiceIface.java
@@ -0,0 +1,6 @@
+package com.webo.deviceandroid.webservice;
+
+public interface WebServiceIface {
+     public static final String BASE_URL = "http://sleepy-cliffs-9910.herokuapp.com/api";
+     //http://mobiapi.herokuapp.com/api
+}
